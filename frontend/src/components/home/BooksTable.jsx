1b1307ddddfa1f4238dcac7cbe1c9b958523227a
Atharv Khardekar <khardekaratharv@gmail.com  git config --global user.name atharvkhardekar>
2024-02-21 17:58:01 +0530

    Fetch Update bookstable

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -5,6 +5,7 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const BooksTable = ({ books }) => {
+  console.log(books);
   return (
     <table className='w-full border-separate border-spacing-2'>
           <thead>
